354d4a1ee83b8cb6ed640c53372d0dc3af00e761
leonelcastelobranco <leoneldvcb@gmail.com>
2024-08-21 11:18:44 -0300

    java operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,18 @@
+public class Operadores {
+    public static void main(String[] args) {    
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras");
+        }  
+        
+        if(condicao1 || condicao2){
+            System.out.println("Umas das condicões são verdadeiras");
+
+        }
+            System.out.println("fim");
+    }
+    
+}
