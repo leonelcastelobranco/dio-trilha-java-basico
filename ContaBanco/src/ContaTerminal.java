bb28e48eca0d339d2b19691b03b6a429044a59c3
leonelcastelobranco <leoneldvcb@gmail.com>
2024-08-22 07:43:54 -0300

    ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        // Criação de um objeto Scanner para capturar a entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitação e captura dos dados do usuário
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.next();
+
+        // Consumir a quebra de linha que fica após a leitura de um número
+        scanner.nextLine(); 
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial:");
+        double saldo = scanner.nextDouble();
+
+        // Exibição da mensagem final
+        String mensagem = "Olá " + nomeCliente + ", Obrigado por criar uma conta no LDBANK, sua agência é " + 
+                          agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+        
+        System.out.println(mensagem);
+
+        // Fechar o scanner
+        scanner.close();
     }
 }
